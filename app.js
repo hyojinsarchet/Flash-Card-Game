0330d5b73f3d8946cf023e93a0cd555532ba8466
hyojin.sarchet <hyojin5931@hanmail.net>
2018-02-12 12:03:54 -0500

    created model and pseudo code on js

--- app.js
+++ app.js
@@ -1,1 +1,75 @@
-console.log("Hello World!")
+////////////////////////  Model   /////////////////////
+var flashcards = [
+      {
+        question: "안녕하세요!",
+        answer: "Hello! [An-nyeong-ha-se-yo]"
+      },
+      {
+        question: "친구",
+        answer: "Friend [Chin-gu]"
+      },
+      {
+        question: "대한민국",
+        answer: "South Korea [Dae-han-min-guk]"
+      },
+      {
+        question: "감사합니다.",
+        answer: "Thank you. [Gam-sa-hap-ni-da]"
+      },
+      {
+        question: "반갑습니다",
+        answer: "Nice to meet you. [Ban-gap-seup-ni-da]"
+      },
+      {
+        question: "소주",
+        answer: "Soju"
+      },
+      {
+        question: "평창",
+        answer: "Pyongchang"
+      },
+      {
+        question: "올림픽",
+        answer: "Olympc"
+      },
+      {
+        question: "제너럴 어셈블리",
+        answer: "General Assembly"
+      },
+      {
+        question: "맥주",
+        answer: "Beer [Maek-ju]"
+      }
+]
+
+
+//How to add the questions and answer??
+//using innerText?
+
+
+// When I click 'correct_button'
+// add currentScore ++1
+// and show Answer
+
+
+// When I click 'wrong_button'
+// just show answer
+
+
+//score function
+// when 'correct_button' is clicked ++1
+// when score reaches 5 - > add message "You've reached today's goal!"
+// maybe using alert? or add color? flash sth? Think about brilliant Idea!
+
+
+// When I click 'next_button',
+// show next question + empty answer box
+
+
+// When I click 'reset_button' reset!
+
+
+// Add addEventListener on Buttons!
+// button.addEventListener("mouseover", function () {
+//     body.classList.add('buttonStyle')
+// })
