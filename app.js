3a04157aa4226fdbec4640ab3c725b40310715da
hyojin.sarchet <hyojin5931@hanmail.net>
2018-02-13 15:31:46 -0500

    tried to make the current score works

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ var currentQuestion = document.querySelector('.currentQuestion')
 var answer = document.querySelector('.answer')
 var currentAnswer = document.querySelector('.currentAnswer')
 var score = document.querySelector('.score')
-var currentScore = parseFloat(document.querySelector('.currentScore').value)
+var currentScore = parseFloat(document.querySelector('.currentScore'))
 var correct = document.querySelector('.correct_button')
 var wrong = document.querySelector('.wrong_button')
 var next = document.querySelector('.next_button')
@@ -57,8 +57,7 @@ var reset = document.querySelector('.reset_button')
 currentScore = 0
 
 
-// default set?
-// So can be used for reset as well.
+// default set & also used for reset button
 window.onload = SetDefaultValue();
 function SetDefaultValue() {
     currentQuestion.innerText = flashCards[0].question
@@ -66,44 +65,43 @@ function SetDefaultValue() {
 }
 
 
-// When 'next_button' is clicked
-// show next question + empty answer box
-// So far it works only once, doesn't empty
-next.addEventListener('click', () => {
-
-      for(i = 1; i <= flashCards.length; i++) {
-          return currentQuestion.innerText = flashCards[i].question
-          currentAnswer.innerText = ''
-      }
-})
-
-// When 'correct_button' is clicked
-// add currentScore ++1
-// and show Answer
+// When 'correct_button' is clicked -> show Answer & add currentScore ++1
+// Q: But now it works only once and gets error & doesn't add score
 correct.addEventListener('click', () => {
     // when score reaches 5 - > add message "You've reached today's goal!"
 
-      for(i = 0; i <= flashCards.length; i++) {
+      for(var i = 0; i <= (flashCards.length - 1); i++) {
           return currentAnswer.innerText = flashCards[i].answer
           // return currentScore.innerText += 1
           currentScore += 1
-          currentScore = currentScore.innerText
+          currentScore.innerText = currentScore
       }
 })
 
 
-// When 'wrong_button' is clicked
-// just show answer
-// So far it works only once
+// When 'wrong_button' is clicked -> just show answer
+// Q: But so far it works only once and stops
+// Q: When I remove 'return' it gives a random answer
 wrong.addEventListener('click', () => {
 
-    for(i = 0; i <= flashCards.length; i++) {
+    for(var i = 0; i <= flashCards.length; i++) {
         return currentAnswer.innerText = flashCards[i].answer
     }
 })
 
 
-// When 'reset_button' is clicked reset!
+// When 'next_button' is clicked -> show next question + empty answer box
+// Q: But now, it works only once and doesn't empty answer box
+next.addEventListener('click', () => {
+
+      for(var i = 1; i <= flashCards.length; i++) {
+          currentQuestion.innerText = flashCards[i].question
+          currentAnswer.innerText = ''
+      }
+})
+
+
+// When 'reset_button' is clicked it resets.
 reset.addEventListener('click', () => {
     SetDefaultValue()
 })
@@ -114,5 +112,5 @@ reset.addEventListener('click', () => {
 //     body.classList.add('buttonStyle')
 // })
 
-// Learn about default value from this link
+// Learn about default value from this link below:
 // https://www.sitepoint.com/community/t/how-to-set-default-value/6914
