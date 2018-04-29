635a4b16ad846d4f64c6f86c286e3c8680520852
hyojin.sarchet <hyojin5931@hanmail.net>
2018-04-28 20:41:25 -0400

    added README and deleted dead codes

--- README.md
+++ README.md
@@ -1,29 +1,30 @@
 # Flash-Card-Game
 
-
 ### About the project
-- This is the first project in the Web Development Immersive Program from GA.
-- This game consists of a front and back of flash cards which are in English and Korean.
-- Users can learn some basic Korean words while playing this game.
 
+* This is the first project in the Web Development Immersive Program from GA.
+* This game consists of a front and back of flash cards which are in English and Korean.
+* Users can learn some basic Korean words while playing this game.
 
 ### How to play this 'Flash Card Game'
-- Read the question on the top flash card.
-- If you know the answer click 'Correct', if not, click 'Wrong'.
-- If you click the 'Correct' button, it will show the answer and the score will go up.
-- If you click the 'Wrong' button, it will just show the answer.
-- If you want to move on to the next question, click 'Next' button.
-- If you want to start over, click 'Reset' button.
-- If you reach certain score, an image with congratulations will pop up.
 
+* Read the question on the top flash card.
+* If you know the answer click 'Correct', if not, click 'Wrong'.
+* If you click the 'Correct' button, it will show the answer and the score will go up.
+* If you click the 'Wrong' button, it will just show the answer.
+* If you want to move on to the next question, click 'Next' button.
+* If you want to start over, click 'Reset' button.
+* If you reach certain score, an image with congratulations will pop up.
 
 ### How to run
-- Clone this repository.
-- Open index.html. (Using Chrome is recommended)
-- Enjoy the game!
 
+* Clone this repository.
+* Open index.html (Using Chrome is recommended)
+* Or click [here](https://hyojinsarchet.github.io/Flash-Card-Game/).
+* Enjoy the game!
 
-### Used tools in the project
- - Html
- - CSS
- - JavaScript
+### Built with
+
+* Html
+* CSS
+* JavaScript
--- app.js
+++ app.js
@@ -2,182 +2,145 @@
 
 // flasgCards array contains 15 questions and answers.
 var flashCards = [
-      {
-        question: "Hello!",
-        answer: "안녕! [An-nyeong]"
-      },
-      {
-        question: "Friend",
-        answer: "친구 [Chin-gu]"
-      },
-      {
-        question: "South Korea",
-        answer: "대한민국 [Dae-han-min-guk]"
-      },
-      {
-        question: "Thank you.",
-        answer: "감사합니다. [Gam-sa-hap-ni-da]"
-      },
-      {
-        question: "Nice to meet you.",
-        answer: "반갑습니다. [Ban-gap-seup-ni-da]"
-      },
-      {
-        question: "Soju",
-        answer: "소주 [soju]"
-      },
-      {
-        question: "Pyongchang",
-        answer: "평창 [Pyong-chang]"
-      },
-      {
-        question: "Olympic",
-        answer: "올림픽 [Ol-lym-pic]"
-      },
-      {
-        question: "dog",
-        answer: "개 [gae]"
-      },
-      {
-        question: "Beer",
-        answer: "맥주 [Maek-ju]"
-      },
-      {
-        question: "Party",
-        answer: "파티 [Pa-ti]"
-      },
-      {
-        question: "Coding",
-        answer: "코딩 [Co-ding]"
-      },
-      {
-        question: "Beer",
-        answer: "맥주 [Maek-ju]"
-      },
-      {
-        question: "Bag",
-        answer: "가방 [Ga-bang]"
-      },
-      {
-        question: "Paper",
-        answer: "종이 [Jong-i]"
-      },
-      {
-        question: "Pencil",
-        answer: "연필 [Yeon-pil]"
-      }
-]
+  {
+    question: "Hello!",
+    answer: "안녕! [An-nyeong]"
+  },
+  {
+    question: "Friend",
+    answer: "친구 [Chin-gu]"
+  },
+  {
+    question: "South Korea",
+    answer: "대한민국 [Dae-han-min-guk]"
+  },
+  {
+    question: "Thank you.",
+    answer: "감사합니다. [Gam-sa-hap-ni-da]"
+  },
+  {
+    question: "Nice to meet you.",
+    answer: "반갑습니다. [Ban-gap-seup-ni-da]"
+  },
+  {
+    question: "Soju",
+    answer: "소주 [soju]"
+  },
+  {
+    question: "Pyongchang",
+    answer: "평창 [Pyong-chang]"
+  },
+  {
+    question: "Olympic",
+    answer: "올림픽 [Ol-lym-pic]"
+  },
+  {
+    question: "dog",
+    answer: "개 [gae]"
+  },
+  {
+    question: "Beer",
+    answer: "맥주 [Maek-ju]"
+  },
+  {
+    question: "Party",
+    answer: "파티 [Pa-ti]"
+  },
+  {
+    question: "Coding",
+    answer: "코딩 [Co-ding]"
+  },
+  {
+    question: "Beer",
+    answer: "맥주 [Maek-ju]"
+  },
+  {
+    question: "Bag",
+    answer: "가방 [Ga-bang]"
+  },
+  {
+    question: "Paper",
+    answer: "종이 [Jong-i]"
+  },
+  {
+    question: "Pencil",
+    answer: "연필 [Yeon-pil]"
+  }
+];
 
+var container = document.querySelector(".container");
+var question = document.querySelector(".question");
+var currentQuestion = document.querySelector(".currentQuestion");
+var answer = document.querySelector(".answer");
+var currentAnswer = document.querySelector(".currentAnswer");
+var score = document.querySelector(".score");
+var currentScoreEl = document.querySelector(".currentScore");
+var correct = document.querySelector(".correct_button");
+var wrong = document.querySelector(".wrong_button");
+var next = document.querySelector(".next_button");
+var reset = document.querySelector(".reset_button");
+var button = document.getElementsByClassName("button");
+var clapping = document.querySelector(".clapping");
 
-var container = document.querySelector('.container')
-var question = document.querySelector('.question')
-var currentQuestion = document.querySelector('.currentQuestion')
-var answer = document.querySelector('.answer')
-var currentAnswer = document.querySelector('.currentAnswer')
-var score = document.querySelector('.score')
-var currentScoreEl = document.querySelector('.currentScore')
-var correct = document.querySelector('.correct_button')
-var wrong = document.querySelector('.wrong_button')
-var next = document.querySelector('.next_button')
-var reset = document.querySelector('.reset_button')
-var button = document.getElementsByClassName('button')
-var clapping = document.querySelector('.clapping')
-
-var currentScore = 0
-var counter = 0
-
+var currentScore = 0;
+var counter = 0;
 
 //////////////////////// ViewModel ////////////////////////
 
 // used for default set & reset button
 window.onload = SetDefaultValue();
 function SetDefaultValue() {
+  counter = 0;
 
-    counter = 0
+  currentQuestion.innerText = flashCards[counter].question;
+  currentAnswer.innerText = flashCards[counter].answer;
 
-    currentQuestion.innerText = flashCards[counter].question
-    currentAnswer.innerText = flashCards[counter].answer
-
-    currentAnswer.innerText = ''
-    currentScore = 0
-    currentScoreEl.innerText = currentScore
+  currentAnswer.innerText = "";
+  currentScore = 0;
+  currentScoreEl.innerText = currentScore;
 }
 
-
 // When 'correct_button' is clicked ->
-correct.addEventListener('click', () => {
+correct.addEventListener("click", () => {
+  // CurrentScore is added (+1).
+  currentScore++;
+  currentScoreEl.innerText = currentScore;
 
-    // CurrentScore is added (+1).
-    currentScore ++
-    currentScoreEl.innerText = currentScore
+  // Answer shows up.
+  counter++;
+  currentAnswer.innerText = flashCards[counter - 1].answer;
 
-    // Answer shows up.
-    counter ++
-    currentAnswer.innerText = flashCards[counter - 1].answer
+  // When reached your goal you get the cute image popup.
+  if (currentScore === 5) {
+    currentQuestion.innerHTML = "";
+    currentAnswer.innerHTML = "";
+    clapping.style.display = "block";
+    container.appendChild(clapping);
 
-    // When reached your goal you get the cute image popup.
-    if(currentScore === 5) {
-        currentQuestion.innerHTML = ''
-        currentAnswer.innerHTML = ''
-        clapping.style.display = 'block'
-        container.appendChild(clapping)
-
-        // When the popup image is clicked it disappers.
-        clapping.onclick = function() {
-            clapping.style.display = "none"
-        }
-    }
-
-    correct.disabled = true
-    wrong.disabled = true
-    // for(var i = 0; i < flashCards.length; i++) {
-    //     if(flashCards[i].question.length === flashCards[i].answer.length) {
-    //         wrong.disabled = true
-    //     }
-    //     else {
-    //         wrong.disabled = false
-    //     }
-    // }
-})
-
+    // When the popup image is clicked it disappers.
+    clapping.onclick = function() {
+      clapping.style.display = "none";
+    };
+  }
+});
 
 // When 'wrong_button' is clicked -> just show answer.
-wrong.addEventListener('click', () => {
-
-    currentAnswer.innerText = flashCards[counter - 1].answer
-    counter ++
-
-    // for(var i = 0; i < flashCards.length; i++) {
-    //     if(flashCards[i].question.length === flashCards[i].answer.length) {
-    //         correct.disabled = true
-    //     }
-    //     else {
-    //         correct.disabled = false
-    //     }
-    // }
-    correct.disabled = true
-    wrong.disabled = true
-})
-
+wrong.addEventListener("click", () => {
+  currentAnswer.innerText = flashCards[counter - 1].answer;
+  counter++;
+});
 
 // When 'next_button' is clicked -> show next question + empty answer box.
-next.addEventListener('click', () => {
-
-    // counter++
-    currentQuestion.innerText = flashCards[counter].question
-    currentAnswer.innerText = ''
-
-    correct.disabled = false
-    wrong.disabled = false
-})
-
+next.addEventListener("click", () => {
+  // counter++
+  currentQuestion.innerText = flashCards[counter].question;
+  currentAnswer.innerText = "";
+});
 
 // When 'reset_button' is clicked it resets.
-reset.addEventListener('click', () => {
-    SetDefaultValue()
-})
-
-
+reset.addEventListener("click", () => {
+  SetDefaultValue();
+});
 
 // Learned about 'default value' from this link below:
 // https://www.sitepoint.com/community/t/how-to-set-default-value/6914
